87dda2a300cb4c29fe4e97f62068e5df89cc97b7
Alexandre Pereira <ale@MacBook-Air-de-Alexandre.local>
2026-02-21 19:36:56 -0300

    build: Update to wppconnect 1.38.0

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import { ServerOptions } from './types/ServerOptions';
 
 export default {
-  secretKey: 'THISISMYSECURETOKEN',
+  secretKey: 'ESTETICDOCTOR_SECRET_2026',
   host: 'http://localhost',
   port: '21465',
   deviceName: 'WppConnect',
